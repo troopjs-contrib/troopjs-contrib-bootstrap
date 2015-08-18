6c569684ed7944d21ba8e0f3a75c6f5ca1ba9a12
Garry Yao <yaojun85@gmail.com>
2015-08-18 18:40:35 +0800

    toggle the checkbox only when "data-toggle" presents on checkbox element

--- checkbox-dropdown/widget.js
+++ checkbox-dropdown/widget.js
@@ -1,5 +1,7 @@
 define([ "troopjs-dom/component/widget" ], function (Widget) {
 	var $ELEMENT = "$element";
+	var UNDEFINED;
+
 
 	return Widget.extend({
 		"dom:input/change": function ($event) {
@@ -27,9 +29,12 @@ define([ "troopjs-dom/component/widget" ], function (Widget) {
 			var $input = $($event.target)
 					.prev("label")
 					.find("input");
-			$input
-				.prop("checked", !$input.prop("checked"))
-				.change();
+
+			var isChecked =
+				$input.data('toggle') !== UNDEFINED?
+				!$input.prop("checked") : true;
+
+			$input.prop("checked", isChecked).change();
 		}
 	});
 });
